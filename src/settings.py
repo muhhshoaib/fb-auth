e9b9ac14e61bb83719890098ca3b68e73826dbb1
Shoaib <shk.mshoaib@gmail.com>
2019-06-04 02:40:35 +0400

    setup django static urls .

--- src/settings.py
+++ src/settings.py
@@ -137,8 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
 
 # try:
 #     from .local import *
--- src/urls.py
+++ src/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from src.apps.fbapp import views
@@ -28,3 +28,4 @@ urlpatterns = [
     path("", views.home, name="home"),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
