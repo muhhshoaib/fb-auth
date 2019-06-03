e1fdee7925ce4dc89ea18e69348b83952326757c
Shoaib <shk.mshoaib@gmail.com>
2019-06-04 02:39:29 +0400

    create default login, logout and home view url routes.

--- src/urls.py
+++ src/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from src.apps.fbapp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("login/", views.login, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('social-auth/', include('social_django.urls', namespace="social")),
+    path("", views.home, name="home"),
 ]
+
